e051b64050d10a75bfaa7da25263e8b3f89e2ed5
yasarosmankhan <yasar_osman@live.co.uk>
2023-10-23 17:48:22 +0100

    Listing all articles on home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
-import Topics from './topics/Topics';
-import NavBar from './NavBar';
+import { Link } from 'react-router-dom';
+import Topics from './Topics';
 
 const Header = () => {
 	return (
 		<>
 			<h1>NC News</h1>
-			<NavBar />
+			<nav className="navbar navbar-expand-lg bg-body-tertiary">
+				<div className="container-fluid">
+					<Link className="navbar-brand" to="/">
+						Home
+					</Link>
+
+					<ul className="navbar-nav ml-auto">
+						{' '}
+						<li className="nav-item">
+							<Link className="btn btn-outline-success" to="/login">
+								Login
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
 			<Topics />
 		</>
 	);
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from 'react';
+import fetchArticles from './api/fetchArticles';
+
 const HomePage = () => {
+	const [articlesList, setArticlesList] = useState([]);
+
+	useEffect(() => {
+		fetchArticles().then(({ articles: articles }) => {
+			const articlesArr = [];
+			articles.map((articles) => {
+				articlesArr.push(articles);
+			});
+			setArticlesList(articlesArr);
+		});
+	}, []);
+
 	return (
 		<>
-			<h1>Home Page</h1>
+			<h1>Articles</h1>
+			<div className="row">
+				<div className="col-8">
+					<div className="row">
+						{articlesList.map((article) => {
+							return (
+								<div className="col" key={article.article_id}>
+									<div className="card" style={{ width: '18rem' }}>
+										<img
+											className="card-img-top"
+											src={article.article_img_url}
+											alt={'topic image'}
+										></img>
+										<div className="card-body">
+											<h5 className="card-title">{article.title}</h5>
+										</div>
+										<ul className="list-group list-group-flush">
+											<li className="list-group-item">
+												Topic: {article.topic}
+											</li>
+											<li className="list-group-item">
+												Author: {article.author}
+											</li>
+											<li className="list-group-item">
+												votes: {article.votes}
+											</li>
+											<li className="list-group-item">
+												comments: {article.comments_count}
+											</li>
+										</ul>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
